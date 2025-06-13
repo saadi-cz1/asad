23b811b194feadf3dafca0bc12052733e315b556
saadi-cz1 <167079756+saadi-cz1@users.noreply.github.com>
2025-06-13 01:16:26 -0700

    Add files via upload

---
+++ rag - Copy.py
@@ -0,0 +1,14 @@
+# Ask the user to enter the first number
+num1 = float(input("Enter the first number: "))
+
+# Ask the user to enter the second number
+num2 = float(input("Enter the second number: "))
+
+# Calculate the sum
+product_result = num1 * num2
+
+# Calculate the average
+difference = num1-num2
+# Print the results
+print("product of the two numbers:", product_result)
+print("Difference of the two numbers:", difference)
