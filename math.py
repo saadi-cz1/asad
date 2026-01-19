8bdfd226a1eb787b03358b4d7d7eb1a2db652681
saadi-cz1 <167079756+saadi-cz1@users.noreply.github.com>
2026-01-19 10:47:02 +0500

    Change function to calculate product and average
    Replaced sum calculation with product calculation and updated function name.

--- math.py
+++ math.py
@@ -1,15 +1,16 @@
-def calculate_sum_and_average():
+def calculate_product_and_average():
    
     num1 = float(input("Enter the first number: "))
 
     num2 = float(input("Enter the second number: "))
 
-    sum_result = num1 + num2
+    prod_result = num1 * num2
 
     average = sum_result / 2
 
-    print("Sum of the two numbers:", sum_result)
+    print("Product of the two numbers:", prod_result)
     print("Average of the two numbers:", average)
 
 if __name__ == "__main__":
     calculate_sum_and_average()
+
