2697c6eb8bd3bdb6ba3cdc9ccc71e07c28509f30
saadi-cz1 <167079756+saadi-cz1@users.noreply.github.com>
2026-03-30 14:48:04 +0500

    Fix calculation logic in sum and average function

--- math.py
+++ math.py
@@ -4,9 +4,9 @@ def calculate_sum_and_average():
 
     num2 = float(input("Enter the second number: "))
 
-    sum_result = num1 / num2
+    sum_result = num1 * num2
 
-    average = sum_result + 2
+    average = sum_result / 2
 
     print("Sum of the two numbers:", sum_result)
     print("Average of the two numbers:", average)
